41da12da2f0f6bbeed85d7656dab771a68b56c6c
Coronel Whatsapp <rafanouses@gmail.com>
2023-05-29 20:36:37 -0300

    Treinando Arrays

---
+++ arrays-loops.js
@@ -0,0 +1,8 @@
+const nomesDeEstudantes = ['Rafael','Augusto','Caio','Leo','Bruno'];
+
+console.log(nomesDeEstudantes);
+console.log(nomesDeEstudantes[4]);
+console.log(nomesDeEstudantes[3]);
+console.log(nomesDeEstudantes[2]);
+console.log(nomesDeEstudantes[1]);
+console.log(nomesDeEstudantes[0]);
